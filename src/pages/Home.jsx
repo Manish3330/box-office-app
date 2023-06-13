1f9ebac56ac038c37af3cff3256834866884241d
Manish Prajapati <mkprajapati2002@gmail.com>
2023-06-14 00:39:02 +0530

    added actors and displayed search result

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,88 @@
 import { useState } from 'react';
-// import { Link } from 'react-router-dom';
-import { searchForShows } from '../api/tvmaze';
+import { searchForShows, searchForPeople } from './../api/tvmaze';
 
 const Home = () => {
-  const [searchString, setsearchString] = useState('');
-  const [apiData, setapiData] = useState([]);
-  const [apiDataError, setapiDataError] = useState(null);
-  const onInputChange = ev => {
-    setsearchString(ev.target.value);
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const onSearchInputChange = ev => {
+    setSearchStr(ev.target.value);
   };
+
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
   const onSearch = async ev => {
     ev.preventDefault();
+
     try {
-      setapiDataError(null);
-       const result = await searchForShows(searchString);
-    setapiData(result);
+      setApiDataError(null);
+
+      if (searchOption === 'shows') {
+        const result = await searchForShows(searchStr);
+        setApiData(result);
+      } else {
+        const result = await searchForPeople(searchStr);
+        setApiData(result);
+      }
     } catch (error) {
-      setapiDataError(error);
+      setApiDataError(error);
     }
-   
   };
+
   const renderApiData = () => {
-    if(apiDataError)
-    {
-      return <div>error occured: {apiDataError.message}</div>
+    if (apiDataError) {
+      return <div>Error occured: {apiDataError.message}</div>;
     }
-    if(apiData)
-      return apiData.map(data => <div key={data.show.id}>{data.show.name}</div>);
-    return null
+
+    if (apiData) {
+      if(apiData && apiData.length>0)
+      return apiData[0].show
+        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+        : apiData.map(data => (
+            <div key={data.person.id}>{data.person.name}</div>
+          ));
+    }
+
+    return null;
   };
+
   return (
     <div>
       <form onSubmit={onSearch}>
-        <input type="text" value={searchString} onChange={onInputChange} />
-        <button type="submit">Update the input</button>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+
+        <label>
+          Shows
+          <input
+            type="radio"
+            name="search-option"
+            value="shows"
+            checked={searchOption === 'shows'}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label>
+          Actors
+          <input
+            type="radio"
+            name="search-option"
+            value="actors"
+            checked={searchOption === 'actors'}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <button type="submit">Search</button>
       </form>
+
       <div>{renderApiData()}</div>
     </div>
   );
 };
+
 export default Home;
