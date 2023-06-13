fb9d51fe414bce93c1ab5f11ee61016bab1d3055
Manish Prajapati <mkprajapati2002@gmail.com>
2023-06-13 18:15:44 +0530

    fetched data from the tv api and display it. and create reusable fetch function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,33 @@
 import { useState } from 'react';
 // import { Link } from 'react-router-dom';
+import { searchForShows } from '../api/tvmaze';
+
 const Home = () => {
   const [searchString, setsearchString] = useState('');
-
+  const [apiData, setapiData] = useState([]);
+  const [apiDataError, setapiDataError] = useState(null);
   const onInputChange = ev => {
     setsearchString(ev.target.value);
   };
   const onSearch = async ev => {
     ev.preventDefault();
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${searchString}`
-    );
-    const body = await response.json();
-    console.log(body);
+    try {
+      setapiDataError(null);
+       const result = await searchForShows(searchString);
+    setapiData(result);
+    } catch (error) {
+      setapiDataError(error);
+    }
+   
+  };
+  const renderApiData = () => {
+    if(apiDataError)
+    {
+      return <div>error occured: {apiDataError.message}</div>
+    }
+    if(apiData)
+      return apiData.map(data => <div key={data.show.id}>{data.show.name}</div>);
+    return null
   };
   return (
     <div>
@@ -20,6 +35,7 @@ const Home = () => {
         <input type="text" value={searchString} onChange={onInputChange} />
         <button type="submit">Update the input</button>
       </form>
+      <div>{renderApiData()}</div>
     </div>
   );
 };
