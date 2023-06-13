58e8be7e77cfdf18b856672ce8e60276168350c9
Manish Prajapati <mkprajapati2002@gmail.com>
2023-06-13 16:41:07 +0530

    Added fetch feature using async await

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,26 @@
-import {Link} from "react-router-dom"
-const Home = ()=>{
-    return (<div><Link to ="/starred">Go to Starred</Link></div>)
-    }
-    export default Home
+import { useState } from 'react';
+// import { Link } from 'react-router-dom';
+const Home = () => {
+  const [searchString, setsearchString] = useState('');
+
+  const onInputChange = ev => {
+    setsearchString(ev.target.value);
+  };
+  const onSearch = async ev => {
+    ev.preventDefault();
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchString}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchString} onChange={onInputChange} />
+        <button type="submit">Update the input</button>
+      </form>
+    </div>
+  );
+};
+export default Home;
