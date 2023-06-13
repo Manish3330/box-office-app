bc81accdc71416888ddbf9c8d73b149471f7c6b0
Manish Prajapati <mkprajapati2002@gmail.com>
2023-06-13 15:21:31 +0530

    Added home and starred pages and learnt React router and setup the Main Layout that will be common in our home and starred component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Starred from './pages/Starred.jsx';
-
+import MainLayout from './components/MainLayout';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<MainLayout/>}>
         <Route path="/" element={<Home />}></Route>
         <Route path="/starred" element={<Starred />}></Route>
+        </Route>
         <Route path="*" element={<div>Not found</div>}></Route>
       </Routes>
     </BrowserRouter>
