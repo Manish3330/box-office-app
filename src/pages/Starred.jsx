65cc688abeb0db2a9689251f0eda89c313eefe33
Manish Prajapati <mkprajapati2002@gmail.com>
2023-07-07 18:46:17 +0530

    created useStarredShows custom hook. placed useStarredShows hook logic in separate file. tell user wheter show is starred or not

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,4 +1,9 @@
-const Starred = ()=>{
-return (<div>Starred</div>)
-}
-export default Starred
+import { useStarredShows } from '../lib/useStarredShows';
+
+const Starred = () => {
+  const [starredShows] = useStarredShows();
+
+  return <div>Starred page, starred {starredShows.length}</div>;
+};
+
+export default Starred;
