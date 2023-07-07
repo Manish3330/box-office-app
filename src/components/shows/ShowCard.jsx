f4e1c35de31976ba9accf28f5d36cdb6a68b0aef
Manish Prajapati <mkprajapati2002@gmail.com>
2023-07-07 18:06:48 +0530

    added initial logic to star shows with useReducer. created usePersisted to sync state with local storage

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,28 +1,23 @@
-
-
-const ShowCard = ({ name, image, id, summary }) => {
+const ShowCard = ({ name, image, id, summary, onStarMeClick }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')
     : 'No description';
-
   return (
     <div>
       <div>
         <img src={image} alt={name} />
       </div>
-
       <h1>{name}</h1>
-
       <p>{summaryStripped}</p>
-
       <div>
-      <a href={`/show/${id}`} target="_blank" rel="noreferrer">
+        <a href={`/show/${id}`} target="_blank" rel="noreferrer">
           Read more
         </a>
-        <button type="button">Star me</button>
+        <button type="button" onClick={() => onStarMeClick(id)}>
+          Star me
+        </button>
       </div>
     </div>
   );
 };
-
 export default ShowCard;
